1cfe71789f5761255e7b880c7d40fd3ed7bd86b2
lexxx <sami.lehtila@gmail.com>
2018-12-28 13:51:31 +0200

    init commit

---
+++ Assets/ship.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour {
+
+    private bool dragging = false;
+    private Rigidbody2D body;
+    public float speed = 10;
+    public Transform nose;
+    public float maxSpeed = 200;
+
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+
+        // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (dragging)
+        {
+            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            transform.up = body.velocity - new Vector2(nose.up.x, nose.up.y);
+            body.AddForce(point * Time.smoothDeltaTime * speed);
+            if (body.velocity.magnitude > maxSpeed)
+            {
+                body.velocity = body.velocity.normalized * maxSpeed;
+            }
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+        }
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+
+    }
+}
