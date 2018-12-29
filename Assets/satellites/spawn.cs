2349e03d52b8162ca4fd57cfd97eea99c56edb93
lexxx <sami.lehtila@gmail.com>
2018-12-29 12:03:53 +0200

    new bgs asteroids to evade

---
+++ Assets/satellites/spawn.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class spawn : MonoBehaviour {
+    public bool rerun = false;
+    public bool destroy = false;
+    private int maxSpawns;
+    public spawnValues values;
+
+
+    void Update()
+    {
+        if (rerun)
+        {
+            rerun = false;
+            if (destroy)
+            {
+                var o = GameObject.FindGameObjectsWithTag("Cube");
+                foreach (var d in o)
+                {
+                    DestroyImmediate(d, true);
+                }
+            }
+            maxSpawns = values.maxSpawns;
+            fill();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        maxSpawns = values.maxSpawns;
+        StartCoroutine(waitAndRespawn());
+        fill();
+    }
+
+    void fill()
+    {
+        for (var l = 0; l < (int)maxSpawns / 1; l ++)
+        {
+            Spawn(Random.Range(-values.x, values.x), Random.Range(0, values.height));
+        }
+    }
+
+    IEnumerator waitAndRespawn()
+    {
+        yield return new WaitForSeconds(values.frequency);
+        Spawn(values.x, Random.Range(0f, values.height));
+        StartCoroutine(waitAndRespawn());
+    }
+
+    private void Spawn(float x, float y)
+    {
+        if (maxSpawns <= 0)
+        {
+            return;
+        }
+        maxSpawns--;
+
+        var heightMultiplier = y * values.heightEffect;
+        var spawned = Instantiate(values.spawnables[Random.Range(0, values.spawnables.Length)]);
+        var scale = Random.Range(values.minScale, values.maxScale) * heightMultiplier;
+        spawned.transform.localScale *= scale;
+        spawned.transform.position = new Vector3(x, y, 0);
+        var body = spawned.GetComponent<Rigidbody>();
+        body.AddForce(Vector3.left * Random.Range(values.minForce, values.maxForce) * heightMultiplier);
+        body.mass = scale * .01f;
+        body.AddTorque(new Vector3(
+            Random.Range(values.minForce, values.maxForce) * heightMultiplier,
+            Random.Range(values.minForce, values.maxForce) * heightMultiplier,
+            Random.Range(values.minForce, values.maxForce) * heightMultiplier
+        ) 
+       );
+    }
+}
---
+++ Assets/ship/ship.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour {
+
+    private bool dragging = false;
+    private Rigidbody body;
+    public float speed = 10;
+    public float maxSpeed = 200;
+    public bool tilt = false;
+    public float tiltTime = 2f;
+
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody>();
+    }
+
+    void OnMouseDown()
+    {
+        dragging = true;
+    }
+
+
+        // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!tilt && Input.GetMouseButtonDown(0))
+        {
+            dragging = true;
+        }
+        if (dragging)
+        {
+            Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.up = body.velocity;
+            body.angularVelocity = Vector3.zero;
+            body.AddForce(point * Time.smoothDeltaTime * speed);
+
+            if (body.velocity.magnitude > maxSpeed)
+            {
+                body.velocity = body.velocity.normalized * maxSpeed;
+            }
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+        }
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+
+    }
+
+    void OnCollisionEnter()
+    {
+        dragging = false;
+        tilt = true;
+        StartCoroutine(waitAndRecover());
+    }
+
+
+    IEnumerator waitAndRecover()
+    {
+        yield return new WaitForSeconds(tiltTime);
+        tilt = false;
+    }
+}
