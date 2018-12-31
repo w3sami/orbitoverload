515183b353328570c50997c9a0af735dfc95b223
lexxx <sami.lehtila@gmail.com>
2018-12-31 16:19:33 +0200

    better size for spawnables. detached camera dep from ship. zoom out by altitude

---
+++ Assets/camera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RoboRyanTron.Unite2017.Variables; // todo remove namespace
+
+public class camera : MonoBehaviour {
+
+    public FloatVariable shipX;
+    public FloatVariable shipY;
+    public spawnValues values;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = new Vector3(shipX.Value, shipY.Value, transform.position.z);
+        Camera.main.orthographicSize = 100 + shipY.Value * 200 / values.height;
+    }
+}
---
+++ Assets/satellites/spawnable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+public class spawnable {
+
+    [SerializeField]
+    public float volume = 0f;
+    public GameObject go;
+    public float sizeNormalized;
+
+    public spawnable (GameObject GO, float vol, float sizeN)
+    {
+        go = GO;
+        volume = vol;
+        sizeNormalized = sizeN;
+    }
+}
