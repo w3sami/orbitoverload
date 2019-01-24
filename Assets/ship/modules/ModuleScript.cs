a333ee884130d6cefa64b94626a78e695ad9b591
Kescay <kescay@kescay.com>
2019-01-24 15:44:54 +0200

    Kinematic rigidbodies and generic modulescripts to grinder, cockpit and storagebox

---
+++ Assets/ship/modules/ModuleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleScript : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+    void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collider " + collision.collider + " hit");
+    }
+}
