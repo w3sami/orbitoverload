e91d66ad83b9ca95f543fd36813a7f935e0fd91d
Kescay <kescay@kescay.com>
2019-01-24 16:58:24 +0200

    Cockpit and grinder proto-explode

--- Assets/ship/modules/ModuleScript.cs
+++ Assets/ship/modules/ModuleScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ModuleScript : MonoBehaviour {
 
-	void Start () {
+    public float durability = 3000;
+    public ParticleSystem destructionExplosion;
+
+    void Start () {
 		
 	}
 	
@@ -14,6 +17,20 @@ public class ModuleScript : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collider " + collision.collider + " hit");
+        Debug.Log("magnitude: " + collision.collider.bounds.size.magnitude + " - velocity: " + collision.relativeVelocity);
+        //Debug.Log("Impact: " + collision.relativeVelocity*collision.collider.bounds.size.magnitude);
+
+        float impact = (collision.relativeVelocity * collision.collider.bounds.size.magnitude).magnitude;
+        Debug.Log("Impact: " + impact);
+
+        if (impact > durability) Explode();
+
+        //Debug.Log("Collider " + collision.collider + " hit");
+    }
+
+    private void Explode()
+    {
+        this.gameObject.SetActive(false);
+        Instantiate(destructionExplosion, transform.position, Quaternion.identity);
     }
 }
